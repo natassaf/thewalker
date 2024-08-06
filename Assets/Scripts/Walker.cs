f4cd9e852bd808f5e4a95cd102a5800e34c98468
Natassa Farmaki <natazf@gmail.com>
2024-08-06 21:04:11 +0300

    smooth basic right turn implemented

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -28,12 +28,14 @@ public class Walker : MonoBehaviour
     private float rightFootLastForwardMovement = 0f;
     private float leftLegDirection=0f;
     private float rightLegDirection=0f;
-    private float leftFootLastRightMovement = 0f;
-    private float rightFootLastRightMovement = 0f;
     private float turnFlag = 0;
     public float turnDuration = 5.0f; // Duration of the rotation in seconds
     private bool isRotating = true;
     private float elapsedTime=0.0f;
+
+    private  Quaternion startRotation = new Quaternion(0f, 0f, 0f, 0f );
+     private  Quaternion rotationChange = new Quaternion(0f, 0f, 0f, 0f );
+
     void Start()
     {
         leftFootTargetOffset = leftFootTarget.localPosition;
@@ -43,70 +45,71 @@ public class Walker : MonoBehaviour
         rightHandTargetOffset = rightHandTarget.localPosition;
     }
 
-    void SetTargetPosition(Transform target, Vector3 offset, float forward, float upwards, float right){
+    void SetTargetPosition(Transform target, Vector3 offset, float forward, float upwards){
         // Performs the mathematical operations to change the target position using the  arguments given
 
         Vector3 moveForward = this.transform.InverseTransformVector(target.forward) * forward;
         Vector3 moveUp = this.transform.InverseTransformVector(target.up) * upwards;
-        Vector3 moveRight = this.transform.InverseTransformVector(target.right) * right;
 
-        target.localPosition = offset + moveForward + moveUp + moveRight;
+        target.localPosition = offset + moveForward + moveUp;
     }
 
-    private void OnCollisionEnter(Collision collided){
-        Debug.Log("Collided with " + collided);
-        this.turnFlag = 1.0f;
-        elapsedTime = 0.0f;
-    }
+    // private void OnCollisionEnter(Collision collided){
+    //     Debug.Log("Collided with " + collided);
+    //     this.turnFlag = 1.0f;
+    //     elapsedTime = 0.0f;
+    // }
 
-    (float, float) moveLeftFootTarget(float adjustedTime, float right){
+    float moveLeftFootTarget(float adjustedTime){
         float forward = legHorizontalCurve.Evaluate(adjustedTime) * 0.3f;
         float upward = legVerticalCurve.Evaluate(adjustedTime+0.5f) * 0.3f;
-        SetTargetPosition(leftFootTarget,  leftFootTargetOffset, forward, upward, right);
+        SetTargetPosition(leftFootTarget,  leftFootTargetOffset, forward, upward);
         float forwardDirection = forward - leftFootLastForwardMovement;
-        float rightDirection = right - leftFootLastRightMovement;
         leftFootLastForwardMovement = forward;
-        leftFootLastRightMovement = right;
-        return (forwardDirection, rightDirection);
+        return forwardDirection;
     }
 
-    (float, float) moveRightFootTarget(float adjustedTime, float right){
+    float moveRightFootTarget(float adjustedTime){
         float forward = legHorizontalCurve.Evaluate(adjustedTime-1) * 0.3f;
         float upward = legVerticalCurve.Evaluate(adjustedTime-0.5f) * 0.3f;
-        SetTargetPosition(rightFootTarget,  rightFootTargetOffset, forward, upward, right);
+        SetTargetPosition(rightFootTarget,  rightFootTargetOffset, forward, upward);
         float forwardDirection = forward - rightFootLastForwardMovement;
-        float rightDirection = right - rightFootLastRightMovement;
-
         rightFootLastForwardMovement = forward;
-        rightFootLastRightMovement = right;
-        return (forwardDirection, rightDirection);
+        return forwardDirection;
 
     }
 
     void moveLeftHandTarget(float adjustedTime){
         float forward = armHorizontalCurve.Evaluate(adjustedTime-1f) * 0.4f;
         float upward = armVerticalCurve.Evaluate(adjustedTime) * 0.01f;
-        float right = 0.0f;
-        SetTargetPosition(leftHandTarget,  leftHandTargetOffset, forward, upward, right);
+        SetTargetPosition(leftHandTarget,  leftHandTargetOffset, forward, upward);
     }
 
     void moveRightHandTarget(float adjustedTime){
         float forward = armHorizontalCurve.Evaluate(adjustedTime) * 0.4f;
         float upward = armVerticalCurve.Evaluate(adjustedTime) * 0.01f;
         float right = 0.0f;
-        SetTargetPosition(rightHandTarget,  rightHandTargetOffset, forward, upward, right);
+        SetTargetPosition(rightHandTarget,  rightHandTargetOffset, forward, upward);
     }
 
     void Update()
     {   
-        
-        float adjustedTime = Time.time * frequency;
-        float right = 0.0f;
-        Debug.Log("turnFlag"+ this.turnFlag);
 
+        float adjustedTime = Time.time * frequency;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && turnFlag==0)
+        { 
+            turnFlag = 1;
+            elapsedTime = 0.0f;
+            rotationChange = Quaternion.AngleAxis(90f, this.transform.up);
+            startRotation = transform.rotation;
+        }
+        
+
+        Debug.Log("turnFlag"+ this.turnFlag);
         // Walking movement
-        (float  leftLegDirectionforward, float leftLegDirectionright)  = moveLeftFootTarget(adjustedTime, right);
-        (float rightLegDirectionforward, float rightLegDirectionright) = moveRightFootTarget(adjustedTime, right);
+        float  leftLegDirectionforward  = moveLeftFootTarget(adjustedTime );
+        float rightLegDirectionforward = moveRightFootTarget(adjustedTime);
         moveLeftHandTarget(adjustedTime);
         moveRightHandTarget(adjustedTime);
 
@@ -122,53 +125,36 @@ public class Walker : MonoBehaviour
         if ( raycastHittingFloor  && leftLegDirectionforward<0){
             leftFootTarget.position = hit.point;
             this.transform.position += this.transform.forward * Math.Max(-leftLegDirectionforward, 0F);
-            // this.transform.position += this.transform.right * leftLegDirectionright;
-            //  if (turnFlag == 1 && elapsedTime<=turnDuration) {
-            //     this.transform.position += this.transform.right * leftLegDirectionforward;
-                
-            //  }
-
         }
         
         raycastHittingFloor = Physics.Raycast(rightFootTarget.position + rightFootTarget.up, -rightFootTarget.up, out hit,  10f);
         if ( raycastHittingFloor && rightLegDirectionforward<0){
             rightFootTarget.position = hit.point;
             this.transform.position += this.transform.forward * Math.Max(-rightLegDirectionforward, 0f);
-            
-            //  if (turnFlag == 1 && elapsedTime<=turnDuration) {
-            //     this.transform.position += this.transform.right * rightLegDirectionforward;
-            //  }
-
-            // this.transform.position += this.transform.right * rightLegDirectionright;
+    
 
         }
-        // if (turnFlag==1){
-        //     StartCoroutine(RotateOverTime(Quaternion.Euler(0, 90, 0), turnDuration));
-        // }
-        // elapsedTime += Time.deltaTime;
-        // if (elapsedTime >turnDuration){
-        //     turnFlag=0;
-        // }
-
+        if (turnFlag==1){
+            StartCoroutine(RotateOverTime(rotationChange, turnDuration, elapsedTime));
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime>turnDuration){
+                // transform.rotation = startRotation * rotationChange;
+                turnFlag=0;
+                elapsedTime=0f;
+            }
+        }
     }
 
-    private IEnumerator RotateOverTime(Quaternion targetRotation, float duration)
+    private IEnumerator RotateOverTime(Quaternion rotationChange, float duration, float elapsedTime)
     {
-        isRotating = true;
-
-        Quaternion startRotation = transform.rotation;
-        float elapsedTimeLocal = 0f;
-
         while (elapsedTime < duration)
         {
-            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, elapsedTimeLocal / duration);
-            elapsedTimeLocal += Time.deltaTime;
-            yield return null; // Wait for the next frame
-        }
+            transform.rotation = Quaternion.Slerp(startRotation, startRotation * rotationChange, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            Debug.Log("elapsedTimeLocal: "+ elapsedTime);
 
-        // Ensure the final rotation is set exactly to the target rotation
-        transform.rotation = targetRotation;
-        isRotating = false;
+            yield return null; 
+        }
     }
 }
 
